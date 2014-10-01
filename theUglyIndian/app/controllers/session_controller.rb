a74e419843b23121244b757a90d689d65b739921
prank7 <prashant.abhishek7g@gmail.com>
2014-10-01 23:17:50 +0530

    Html files

--- theUglyIndian/app/controllers/session_controller.rb
+++ theUglyIndian/app/controllers/session_controller.rb
@@ -14,9 +14,21 @@ class SessionController < ApplicationController
 
   def auth
   	require 'google-id-token'
-    #token = request.headers["Authorization"]
+    token = request.headers["Authorization"]
     validator = GoogleIDToken::Validator.new
     token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE2YmYyZTFmZjU0NjQ4YmI4YzU0YWEzN2ZiNDExYmVhYjQ5MDI2M2MifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiaWQiOiIxMDgxNDA2NjI3NzU2MDE3NTc0MTAiLCJzdWIiOiIxMDgxNDA2NjI3NzU2MDE3NTc0MTAiLCJhenAiOiI4MTQwMzU5OTU4OTAtY3IzcnZ2dGVxcG12cjFtMGxsbmxuZHRwaGI3YnRhM2guYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6InJhamVlZm1rQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdWQiOiI4MTQwMzU5OTU4OTAtODQ4ZWl0NWljZTExbjVqamMwc3JxZzJwZXNmZnAyam8uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJ2ZXJpZmllZF9lbWFpbCI6dHJ1ZSwiY2lkIjoiODE0MDM1OTk1ODkwLWNyM3J2dnRlcXBtdnIxbTBsbG5sbmR0cGhiN2J0YTNoLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiaWF0IjoxNDEyMDY1NjE2LCJleHAiOjE0MTIwNjk1MTZ9.JILrCL__MH44cv5KX_UBOYKMfMDTlRVa92qYIOm0vQnxdZpvY0CaGJ5CZn-3AkBDZg8WWJf7V6PppOGX7qVZXNAJt7x_C6arusvmz1RkJ3rF5ueiVUykx5tu7jfR9pGZ3rkFIWvfJsCWq2qtECwPFbVni19bb6QGysbXVbYbjrc"
+    #token = "ya29.kQAFAWDNDtUWokkLt0rWaA3Dx_bnu8ghZicucxaKYOHBE3Lb27qYeLpe"   accesstoken
+
+    # response = HTTParty.get("https://accounts.google.com/o/oauth2/auth?
+    #  scope=email%20profile&
+    #  state=%2Fprofile&
+    #  redirect_uri=htt&
+    #  response_type=token&
+    #  client_id=814035995890-848eit5ice11n5jjc0srqg2pesffp2jo.apps.googleusercontent.com")
+
+    
+    response_oauth_code = HTTParty.post()
+    
 
 		required_audience ="814035995890-848eit5ice11n5jjc0srqg2pesffp2jo.apps.googleusercontent.com"
 		required_client_id ="814035995890-cr3rvvteqpmvr1m0llnlndtphb7bta3h.apps.googleusercontent.com"
@@ -29,8 +41,30 @@ class SessionController < ApplicationController
 		end
 
 
+
+
+
+
+    # url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=#{token}"
+    #render json: jwt
+    # puts url
+    # puts token
+    # response = HTTParty.get("https://www.googleapis.com/oauth2/v1/userinfo",query: {access_token: token})
+    # puts response.body
     render json: jwt
-  end
+
+
+    # require 'oauth2'
+
+    # client = OAuth2::Client.new('814035995890-848eit5ice11n5jjc0srqg2pesffp2jo.apps.googleusercontent.com', 'mhac6RB65RwpBXotqrB1fSXg', :site => 'https://localhost')
+
+    # client.auth_code.authorize_url(:redirect_uri => 'http://localhost:8080/oauth2/callback')
+    # # => "https://example.org/oauth/authorization?response_type=code&client_id=client_id&redirect_uri=http://localhost:8080/oauth2/callback"
+
+    # token = client.auth_code.get_token('authorization_code_value', :redirect_uri => 'http://localhost:8080/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
+    # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+
+  end 
 
   def destroy
     session[:user_id] = nil
@@ -38,3 +72,6 @@ class SessionController < ApplicationController
   end
 
 end
+
+
+
---
+++ theuglyindian-html/readme.txt
@@ -0,0 +1,22 @@
+This app has a web client and an android client. The backend is being developed in ruby on rails.
+
+The following description is the rough flow of the web client.
+
+1. A user visits the site(starting with index.html), he can login to app using google oauth.
+2. After login he will reach his dashboard, in navigation bar of the dashboard there are options to 
+     a) Initiate a Spotfix - Clicking this will take you to page where you can initiate a spotfix by giving details like location, description and uploding images etc. After initiating the spotfix it will be sent to admin for an approval, if admin approves then it will start featuring in the "Upcomin Spotfixes" section.
+
+     b) Upcoming Spotfixes - takes you to a page which lists all upcoming spotfixes. Clicking on any upcoming Spotfixes will take you to the individual spotfix description page. You can see the details and join them. You can also search for a spotfix.
+
+     c) My Spotfixes- these are spotfixes that you initiated. There are spotfixes that are done in past and then spotfixes that you have initiated and are Upcoming. clicking on indiviual spotfixes will take you to their pages and will give you option to edit the spotfix. 
+
+     d) logout -Would do the most obvious thing! :)
+
+  Thing on dashboard page are metrics and upcoming spotfixes. metrics are how many spotfixes done. How many hours spent etc.
+
+
+3. Admin view-
+	Visit admin.html (granted that you are set as admin)
+	You will see a section of spotfixes waiting approval. you can visit their individual pages and then approve them.
+	other sections are Upcoming and past spotfixes.
+
